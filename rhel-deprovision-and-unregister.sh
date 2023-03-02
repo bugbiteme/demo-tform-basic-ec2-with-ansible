650c6a6a85dace7850ad1324dacfa0d049df195c
bugbiteme <leon.s.levy@gmail.com>
2023-03-01 17:00:52 -0800

    more enahncements around initial config of RHEL

--- rhel-deprovision-and-unregister.sh
+++ rhel-deprovision-and-unregister.sh
@@ -1,7 +1,7 @@
 
 
 echo "Remove from Insighs and unregister RHEL"
-ansible-playbook -i inventory rhel-unreg-insights.yml 
+ansible-playbook -i inventory rhel-unreg-insights.yml -v
 
 echo "Delete all EC2 instances"
 terraform destroy -auto-approve
--- rhel-provision-and-register.sh
+++ rhel-provision-and-register.sh
@@ -13,4 +13,4 @@ fi
 echo "Waiting 20 seconds to make sure ec2 instances are ready"
 sleep 1 && for i in {20..1}; do echo "$i" && sleep 1; done
 
-ansible-playbook -i inventory rhel-reg-insights.yml --vault-id @prompt
+ansible-playbook -i inventory rhel-reg-insights.yml --vault-id @prompt -v
